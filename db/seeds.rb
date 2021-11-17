fae2589a3fa8fd98b8bc84c5be4b63cd5a3be2b3
EricChomette <eric.chomette@gmail.com>
2021-11-17 10:36:00 +0100

    modif seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 User.destroy_all
 
-benj = User.create!(name: 'benjamin', pseudo: 'BetterCallBen', description: 'le meilleur joueur du 4 cartes', phone_number: '0613653334', address: '41 avenue lamartine', password: "secret", email: 'benjamin@gmail.com')
-eric = User.create!(name: 'eric', pseudo: 'Wagonnet', description: 'le meilleur joueur du ping pong', phone_number: '0623794874', address: '1 rue montaigne', password: "secret", email: 'eric@gmail.com')
-mael = User.create!(name: 'mael', pseudo: 'Xund', description: 'le vrai meilleur joueur de ping pong', phone_number: '0713653334', address: 'Dommartin', password: "secret", email: 'mael@gmail.com')
-robin = User.create!(name: 'robin', pseudo: 'Mauresque', description: 'poker menteur', phone_number: '0642434445', address: 'Montée de fourvière', password: "secret", email: 'robin@gmail.com')
+benj = User.create!(name: 'benjamin', pseudo: 'BetterCallBen', description: 'le meilleur joueur du 4 cartes', phone_number: '0613653334', address: '41 avenue lamartine, Lyon', password: "secret", email: 'benjamin@gmail.com')
+eric = User.create!(name: 'eric', pseudo: 'Wagonnet', description: 'le meilleur joueur du ping pong', phone_number: '0623794874', address: '1 rue montaigne, 42000 Saint Etienne', password: "secret", email: 'eric@gmail.com')
+mael = User.create!(name: 'mael', pseudo: 'Xund', description: 'le vrai meilleur joueur de ping pong', phone_number: '0713653334', address: '25 rue des cordineaux, Dommartin', password: "secret", email: 'mael@gmail.com')
+robin = User.create!(name: 'robin', pseudo: 'Mauresque', description: 'poker menteur', phone_number: '0642434445', address: 'Montée de fourvière, Lyon', password: "secret", email: 'robin@gmail.com')
 
 puts "#{User.count} were created"
 
